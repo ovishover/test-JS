a63c185563caf6cfeceae269daf16a40be6ce881
Nikita Hudz <nikita.gudz8@gmail.com>
2022-04-27 19:06:53 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -537,3 +537,12 @@
 // console.log(account.getTransactionDetails(1));
 // console.log(account.getTransactionDetails(100));
 // console.log(account.getTransactionTotal(Transaction.DEPOSIT));
+
+// const first = { propA: 5, propB: 10, propC: 50 };
+// const second = { propC: 15, propD: 20 };
+
+// const third = { ...first, ...second };
+// console.log(third);
+
+// const fourth = { ...second, ...first };
+// console.log(fourth);
