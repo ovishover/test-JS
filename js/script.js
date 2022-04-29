5e1211292de01c8a8a8d1373bf2ba4ca5e3cb25d
Nikita Hudz <nikita.gudz8@gmail.com>
2022-04-29 10:30:32 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -594,3 +594,31 @@
 // atTheOldToad.removePotion('Dragon breath');
 // atTheOldToad.updatePotionName('Dragon breath', 'Polymorth');
 // console.table(atTheOldToad.getPotions());
+
+function processCall(recipient, onAvailable, onNotAvailable) {
+  // Имитируем доступность абонента случайным числом
+  const isRecipientAvailable = Math.random() > 0.5;
+  if (!isRecipientAvailable) {
+    onNotAvailable(recipient);
+    return;
+  }
+  onAvailable(recipient);
+}
+
+function takeCall(name) {
+  console.log(`Соединяем с ${name}, ожидайте...`);
+  // Логика принятия звонка
+}
+
+function activateAnsweringMachine(name) {
+  console.log(`Абонент ${name} недоступен, оставьте сообщение.`);
+  // Логика активации автоответчика
+}
+
+function leaveHoloMessage(name) {
+  console.log(`Абонент ${name} недоступен, записываем голограмму.`);
+  // Логика записи голограммы
+}
+
+processCall("Манго", takeCall, activateAnsweringMachine);
+processCall("Поли", takeCall, leaveHoloMessage);
