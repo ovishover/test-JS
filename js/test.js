1abc44e6c0d92cc30a22084c39c973dfbf4b0a66
Nikita Hudz <nikita.gudz8@gmail.com>
2022-05-29 19:56:46 +0300

    test buttons

---
+++ js/test.js
@@ -0,0 +1,17 @@
+const tagsContainer = document.querySelector('.js-tags');
+
+tagsContainer.addEventListener('click', onTagsContainerClick);
+
+function onTagsContainerClick(e) {
+  if (e.target.nodeName !== 'BUTTON') {
+    return;
+  }
+
+  const currentActiveBtn = document.querySelector('.tags__btn--active');
+  console.log(currentActiveBtn);
+
+  if (currentActiveBtn) {
+    currentActiveBtn.classList.remove('tags__btn--active');
+  }
+  e.target.classList.add('tags__btn--active');
+}
