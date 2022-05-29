45dff47c29af64f4252bf130de079c6d6e6eb63b
Nikita Hudz <nikita.gudz8@gmail.com>
2022-05-29 20:43:00 +0300

    fix

--- js/test.js
+++ js/test.js
@@ -1,17 +1,36 @@
-const tagsContainer = document.querySelector('.js-tags');
+// const tagsContainer = document.querySelector('.js-tags');
+// let selectedTag = null;
+// tagsContainer.addEventListener('click', onTagsContainerClick);
 
+// function onTagsContainerClick(e) {
+//   if (e.target.nodeName !== 'BUTTON') {
+//     return;
+//   }
+//   const currentActiveBtn = document.querySelector('.tags__btn--active');
+//   currentActiveBtn?.classList.remove('tags__btn--active');
+//   const nextActiveBtn = e.target;
+//   nextActiveBtn.classList.add('tags__btn--active');
+//   selectedTag = nextActiveBtn.dataset.value;
+//   console.log(selectedTag);
+// }
+
+const tagsContainer = document.querySelector('.js-tags');
+let selectedTag = new Set();
 tagsContainer.addEventListener('click', onTagsContainerClick);
 
 function onTagsContainerClick(e) {
   if (e.target.nodeName !== 'BUTTON') {
     return;
   }
+  const btn = e.target;
+  const tag = btn.dataset.value;
+  const isActive = btn.classList.contains('tags__btn--active');
 
-  const currentActiveBtn = document.querySelector('.tags__btn--active');
-  console.log(currentActiveBtn);
-
-  if (currentActiveBtn) {
-    currentActiveBtn.classList.remove('tags__btn--active');
+  if (isActive) {
+    selectedTag.delete(tag);
+  } else {
+    selectedTag.add(tag);
   }
-  e.target.classList.add('tags__btn--active');
+  btn.classList.toggle('tags__btn--active');
+  console.log(selectedTag);
 }
